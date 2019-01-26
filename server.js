4910bfd317ffd9a4b010d3981df71b3f4a85b458
chef <cgcook81@gmail.com>
2019-01-26 14:43:45 -0500

    setup basic express server

---
+++ server.js
@@ -0,0 +1,14 @@
+
+//Dependencies
+//
+const express = require('express');
+const app = express();
+// const methodOverride = require('method-override');
+// const mongoose = require('mongoose');
+// const db = mongoose.connection;
+//
+//
+
+app.listen(3000, () => {
+  console.log('listening...');
+});
