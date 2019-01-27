4dc649d4550501b60331dfea5358bbfeb0b6643f
chef <cgcook81@gmail.com>
2019-01-26 19:28:02 -0500

    added index/show route

---
+++ models/moviedb.js
@@ -0,0 +1,13 @@
+
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  synop: String,
+  favActor: String,
+  rating: { type: Number, min: 0, max: 5, required: true}
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
--- server.js
+++ server.js
@@ -8,16 +8,31 @@ const app = express();
 // const db = mongoose.connection;
 //
 //
-app.get('/', (req, res) => {
-  console.log("Good start to the project");
+const movies = ([
+  {
+    name: 'Jaws',
+    synop: 'A shark stalks a local community off Martha\'s Vineyard',
+    favActor: 'Robert Shaw',
+    rating: 5
+  }
+]);
+
+// app.get('/', (req, res) => {
+//   res.render('index.ejs');
+// });
+
+app.get('/movies', (req, res) => {
+  res.send(movies);
 });
-
-
-
-
-
-
+app.get('/movies/:id', (req, res) => {
+  res.send(movies[req.params.id]);
+});
 
 app.listen(3000, () => {
   console.log('listening...');
 });
+
+// mongoose.connect('mongodb://localhost:27017/shiza', {useNewUrlParser:true});
+// mongoose.connection.once('open', () => {
+//     console.log('connected to mongo');
+// });
